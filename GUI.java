57fa6d821273a132a6a4b36d749effcdb3385cf6
Meg Fredericks <megfredericks@128-223-223-170-wireless.uoregon.edu>
2016-10-07 11:48:13 -0700

    Made changes to GUI, still doesn't work

--- GUI.java
+++ GUI.java
@@ -10,10 +10,15 @@ import javax.swing.*;
 public class GUI {
 	
 	private JFrame frame;
+	private JLabel headerLabel;
+	private JMenuBar menuBar;
+	private JMenu menu, submenu;
+	private JMenuItem menuItem;
 
 	public GUI() {
 		createPanel();
 	}
+
 	/**
 	 * Creates a JFrame that will host the buttons for the address book
 	 * @return	the panel created by this method
@@ -21,9 +26,44 @@ public class GUI {
 	private void createPanel() {
 		// Creating the window for the GUI
 		this.frame = new JFrame("MainWindow");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.frame.setSize(400, 400);
+		
+		// Add component
+		JLabel blank = new JLabel();
+		blank.setOpaque(true);
+		blank.setPreferredSize(new Dimension(400, 400));
+
+		// Creating the menu bar
+		/*menuBar = new JMenuBar();
+
+		// Build "File" menu
+		menu = new JMenu("File");
+		menu.setMnemonic(KeyEvent.VK_A);
+		*/
+		// Create "File" drop-down menu
+		/**
+		String[] fileOptions = {"File", "New", "Open", "Save", "Save As"};
+		final DefaultComboBoxModel defaultOptions = new DefaultComboBoxModel(fileOptions);
+		final JComboBox fileMenu = new JComboBox(defaultOptions);
+		fileMenu.setSelectedIndex(0);
+		
+		JScrollPane fileMenuScrollPane = new JScrollPane(defaultOptions);
+
+		JButton showButton = new JButton("Show");
+	
+		ActionListener e = new ActionListener();
+		showButton.addActionListener(e);
+		frame.add(fileMenuScrollPane);
+		frame.add(showButton);*/
+		
+		// Makes the window visible
 		frame.pack();
 		frame.setVisible(true);
 	}	
+	
+	public static void main(String[] args) {
+		new GUI();
+	}
 
 }
