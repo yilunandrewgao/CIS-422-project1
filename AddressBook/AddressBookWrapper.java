28ea6b5b0aaf8e6d3880fca72eb710228a1767ce
Meg Fredericks <mfreder3@uoregon.edu>
2016-10-13 22:54:45 -0700

    Added error dialogs

--- AddressBook/AddressBookWrapper.java
+++ AddressBook/AddressBookWrapper.java
@@ -46,6 +46,7 @@ public class AddressBookWrapper implements ActionListener {
         GUIController = _GUIController;
 
         displayWindow();
+        initializeContactFieldsComponents();
 
         //code for mouse listener
         //contacts are indexed starting at 1, since the column headers are in row 0
@@ -53,6 +54,9 @@ public class AddressBookWrapper implements ActionListener {
             @Override
             public void mouseClicked(MouseEvent e) {
                 System.out.println(lastContactList.get(addressBookDisplay.rowAtPoint(e.getPoint())).toString());
+
+                //display the detailed contact info on side
+                displayContact(lastContactList.get(addressBookDisplay.rowAtPoint(e.getPoint())));
             }
         });
     }
@@ -114,113 +118,8 @@ public class AddressBookWrapper implements ActionListener {
         this.frame.addWindowListener(cl);
     }
 
-    private Object[][] getAddressBookDisplay() {
-
-        int line_num = 0;
-        ArrayList<AddressEntry> book = this.controller.returnCurrentBook();
-        lastContactList = book;
-        System.out.println(book);
-        Object[][] returnArray = new Object[book.size()][9];
-        for(AddressEntry entry:book) {
-            returnArray[line_num][0] = entry.getFirstName();
-            returnArray[line_num][1] = entry.getLastName();
-            returnArray[line_num][2] = entry.getDelivery();
-            returnArray[line_num][3] = entry.getSecond();
-            returnArray[line_num][4] = entry.getEmail();
-            returnArray[line_num][5] = entry.getPhone();
-            returnArray[line_num][6] = entry.getCity();
-            returnArray[line_num][7] = entry.getState();
-            returnArray[line_num][8] = entry.getZip();
-
-            line_num += 1;
-        }
-
-        return returnArray;
-    }
-
-    public void actionPerformed(ActionEvent e) {
-        // If you select a contact, info will display on side, and there are save and delete options
-        // editable fields
-        if (e.getSource() == newContactOption) {
-            displayNewContact(); // put this on right side of window
-        } else if (e.getSource() == saveOption) {
-            // Controller.save, true or false if saved. display "saved successfully" method
-            // If save failed, display "error while saving"
-
-            try {
-                controller.save();
-            } catch (Exception ex) {
-                System.out.println("Failed to save Address Book.");
-            }
-
-        } else if (e.getSource() == saveAsOption) {
-            // open a file chooser dialog box
-
-            String userFileName = null;
-
-            JFileChooser c = new JFileChooser(System.getProperty("user.dir"));
-            int rVal = c.showSaveDialog(fileMenu);
-            if (rVal == JFileChooser.APPROVE_OPTION) {
-                userFileName = c.getCurrentDirectory().toString() + "/" + c.getSelectedFile().getName();
-            }
-
-            // if user chose a file, call the save as method
-            if (userFileName != null) {
-                try {
-                    controller.saveAs(userFileName);
-                } catch (Exception ex) {
-                    System.out.println("Failed to save Address Book as " + userFileName);
-                }
-            }
-
-        }
-
-        else if (e.getSource() == deleteOption) {
-            boolean deleted;
-            int response = JOptionPane.showConfirmDialog(null, "Are you sure you want to delete this phone book?", "Warning",
-                    JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
-            if (response == JOptionPane.YES_OPTION) {
-                deleted = controller.delete();
-
-                if (deleted == false) {
-                    JOptionPane.showMessageDialog(frame, "Unable to delete address book.");
-                }
-            }
-        }
-        // If the user wants to save a new contact, send info from text fields to controller
-        else if (e.getSource() == newContactSave) {
-            String[] newContactInfo = new String[9];
-            newContactInfo[0] = firstName.getText();
-            newContactInfo[1] = lastName.getText();
-            newContactInfo[2] = address1.getText();
-            newContactInfo[3] = address2.getText();
-            newContactInfo[4] = email.getText();
-            newContactInfo[5] = phone.getText();
-            newContactInfo[6] = city.getText();
-            newContactInfo[7] = state.getText();
-            newContactInfo[8] = zip.getText();
-            try {
-                controller.tryToAddEntry(newContactInfo);
-            } catch (TooLittleInputException ex1) {
-                System.out.println(ex1.getMessage());
-            } catch (InvalidInputException ex2) {
-                System.out.println(ex2.getMessage());
-                System.out.println(ex2.getInvalidFieldNum());
-            }
-            addressBookDisplay = new JTable(getAddressBookDisplay(), columnNames);
-
-            // Removing "add new contact" screen because contact has been added.
-            rightPanel.remove(contactFields);
-            this.frame.pack();
-            this.frame.setVisible(true);
-        } else if (e.getSource() == newContactCancel) {
-
-        }
-
-
-    }
-
-    private void displayNewContact() {
+    // helper method to initialize all widgets in contactFields panel
+    private void initializeContactFieldsComponents() {
         firstName = new JTextField(textFieldDimension);
         lastName = new JTextField(textFieldDimension);
         phone = new JTextField(textFieldDimension);
@@ -258,11 +157,155 @@ public class AddressBookWrapper implements ActionListener {
         newContactCancel.addActionListener(this);
         contactFields.add(newContactSave);
         contactFields.add(newContactCancel);
+    }
+
+    private Object[][] getAddressBookDisplay() {
+
+        int line_num = 0;
+        ArrayList<AddressEntry> book = this.controller.returnCurrentBook();
+        lastContactList = book;
+        System.out.println(book);
+        Object[][] returnArray = new Object[book.size()][9];
+        for(AddressEntry entry:book) {
+            returnArray[line_num][0] = entry.getFirstName();
+            returnArray[line_num][1] = entry.getLastName();
+            returnArray[line_num][2] = entry.getDelivery();
+            returnArray[line_num][3] = entry.getSecond();
+            returnArray[line_num][4] = entry.getEmail();
+            returnArray[line_num][5] = entry.getPhone();
+            returnArray[line_num][6] = entry.getCity();
+            returnArray[line_num][7] = entry.getState();
+            returnArray[line_num][8] = entry.getZip();
+
+            line_num += 1;
+        }
+
+        return returnArray;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        // If you select a contact, info will display on side, and there are save and delete options
+        // editable fields
+        if (e.getSource() == newContactOption) {
+            displayNewContact(); // put this on right side of window
+        } else if (e.getSource() == saveOption) {
+            // Controller.save, true or false if saved. display "saved successfully" method
+            // If save failed, display "error while saving"
+
+            try {
+                controller.save();
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(frame, "Error", "Failed to save Address Book.", JOptionPane.ERROR_MESSAGE);
+                System.out.println("Failed to save Address Book.");
+            }
+
+        } else if (e.getSource() == saveAsOption) {
+            // open a file chooser dialog box
+
+            String userFileName = null;
+
+            JFileChooser c = new JFileChooser(System.getProperty("user.dir"));
+            int rVal = c.showSaveDialog(fileMenu);
+            if (rVal == JFileChooser.APPROVE_OPTION) {
+                userFileName = c.getCurrentDirectory().toString() + "/" + c.getSelectedFile().getName();
+            }
+
+            // if user chose a file, call the save as method
+            if (userFileName != null) {
+                try {
+                    controller.saveAs(userFileName);
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(frame, "Error", "Failed to save Address Book as " + userFileName, JOptionPane.ERROR_MESSAGE);
+                }
+            }
+
+        }
+
+        else if (e.getSource() == deleteOption) {
+            boolean deleted;
+            int response = JOptionPane.showConfirmDialog(null, "Are you sure you want to delete this phone book?", "Warning",
+                    JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+            if (response == JOptionPane.YES_OPTION) {
+                deleted = controller.delete();
+
+                if (deleted == false) {
+                    JOptionPane.showMessageDialog(frame, "Unable to delete address book.");
+                }
+            }
+        }
+        // If the user wants to save a new contact, send info from text fields to controller
+        else if (e.getSource() == newContactSave) {
+            String[] newContactInfo = new String[9];
+            newContactInfo[0] = firstName.getText();
+            newContactInfo[1] = lastName.getText();
+            newContactInfo[2] = address1.getText();
+            newContactInfo[3] = address2.getText();
+            newContactInfo[4] = email.getText();
+            newContactInfo[5] = phone.getText();
+            newContactInfo[6] = city.getText();
+            newContactInfo[7] = state.getText();
+            newContactInfo[8] = zip.getText();
+            try {
+                controller.tryToAddEntry(newContactInfo);
+            } catch (TooLittleInputException ex1) {
+                // Displays "Too little input" message
+                JOptionPane.showMessageDialog(frame, "Error", ex1.getMessage(), JOptionPane.ERROR_MESSAGE);
+            } catch (InvalidInputException ex2) {
+                // Would you still like to save?
+                JOptionPane.showMessageDialog(frame, "Error", ex2.getMessage(), JOptionPane.ERROR_MESSAGE);
+            }
+            addressBookDisplay = new JTable(getAddressBookDisplay(), columnNames);
+
+            // Removing "add new contact" screen because contact has been added.
+            rightPanel.remove(contactFields);
+            this.frame.pack();
+            this.frame.setVisible(true);
+        } else if (e.getSource() == newContactCancel) {
+
+        }
+
+
+    }
+
+    private void displayNewContact() {
+
+        // empty the default text in each JTextField
+        firstName.setText("");
+        lastName.setText("");
+        phone.setText("");
+        address1.setText("");
+        address2.setText("");
+        city.setText("");
+        state.setText("");
+        zip.setText("");
+        email.setText("");
+
+        contactFields.repaint();
 
         rightPanel.add(contactFields);
         this.frame.pack();
         this.frame.setVisible(true);
     }
+
+    // variant of displayNewContact for case where an AddressEntry is provided
+    private void displayContact(AddressEntry entry) {
+        firstName.setText(entry.getFirstName());
+        lastName.setText(entry.getLastName());
+        phone.setText(entry.getPhone());
+        address1.setText(entry.getDelivery());
+        address2.setText(entry.getSecond());
+        city.setText(entry.getCity());
+        state.setText(entry.getState());
+        zip.setText(entry.getZip());
+        email.setText(entry.getEmail());
+
+        contactFields.repaint();
+
+        rightPanel.add(contactFields);
+        this.frame.pack();
+        this.frame.setVisible(true);
+    }
+
 
 
     // the window listener that handles closing
